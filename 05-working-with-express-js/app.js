a714ed074a2a65f3984fb15016b768fa461245e7
Aruminiya <leo80103@gmail.com>
2025-02-18 12:44:49 +0800

    feat: added 404 page

--- 05-working-with-express-js/app.js
+++ 05-working-with-express-js/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const bodyParser = require('body-parser')
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false})) // `body-parser` 是一個用於解析 HTTP 請求主體的中介軟體
+
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 // '/' 代表 ‘/’ 為開頭的路徑
 app.use('/', (req, res, next) => {
@@ -16,19 +23,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.get('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
+// 404 page
+app.use((req, res, next) => {
+  res.status(404);
+  res.send('<h1>Page not found</h1>');
+  next();
 });
 
 app.listen(3000);
